2207927a3f3fc170c9dee59db2519db407831d04
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2022-05-08 23:18:31 +0530

    version error resolved

--- .dart_tool/flutter_build/generated_main.dart
+++ .dart_tool/flutter_build/generated_main.dart
@@ -3,7 +3,7 @@
 // This file is generated from template in file `flutter_tools/lib/src/flutter_plugins.dart`.
 //
 
-// @dart = 2.12
+// @dart = 2.16
 
 // When `package:opp/main.dart` defines `main`, that definition is shadowed by the definition below.
 export 'package:opp/main.dart';
